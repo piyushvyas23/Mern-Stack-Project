e086b7baaa6ded0a703cdee97d3a568d5e437dc4
piyushvyas23 <84887900+piyushvyas23@users.noreply.github.com>
2021-07-02 17:21:03 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="about">
+            
+                <br></br>
+                <br></br>
+                <h4><b>Piyush Vyas</b></h4>
+                <h4><b>Priyanshu Mutha </b></h4>
+            </div>
+
+            
+    )
+}
+export default About
+
